28ec6db49977774e5946ebbc39cda928a04f6b59
just2click <just2click@outlook.com>
2020-06-05 18:32:53 +0300

    Addind Scaffold

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -3,10 +3,25 @@ import "package:flutter/material.dart";
 void main () {
   
   runApp(
-    Center(
-      child: Text(
-          "Hello Flutter", textDirection: TextDirection.ltr
+
+    new MaterialApp(
+      title: "My Flutter App",
+      home: new Scaffold(
+        appBar: AppBar(title: Text("My Flutter App")),
+        body:
+        new Material(
+            color: Colors.lightGreen,
+            child:
+            Center(
+                child: Text(
+                  "Hello Flutter",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(color: Colors.white, fontSize: 40.0),
+                )
+            )
+        )
       )
     )
+
   );
 }
